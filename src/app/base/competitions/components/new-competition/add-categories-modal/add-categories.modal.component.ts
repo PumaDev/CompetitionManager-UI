65106c95432e4abb82984e3549bdd98c141ec365
PumaDev <mihail_puzdrov@mail.ru>
2019-08-30 00:03:52 +0300

    Add filter in add-categoris modal

--- src/app/base/competitions/components/new-competition/add-categories-modal/add-categories.modal.component.ts
+++ src/app/base/competitions/components/new-competition/add-categories-modal/add-categories.modal.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ICompetitionCategory } from '../../../models/category.model';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ICompetitionCategory} from '../../../models/category.model';
+import {FormControl, FormGroup} from '@angular/forms';
+import {deepCloneMerge} from '../../../../../shared/utils/redux.utils';
 
 @Component({
   selector: 'app-add-categories-modal',
@@ -10,25 +12,34 @@ export class AddCategoriesModalComponent implements OnInit {
 
   @Input() set allCategories(allCategories: ICompetitionCategory[]) {
     this._allCategories = allCategories;
-    this.updateCheckedCategories();
+    this.updateAllCheckableCategories();
   }
 
   @Input() set selectedCategories(selectedCategories: ICompetitionCategory[]) {
     this._selectedCategories = selectedCategories;
-    this.updateCheckedCategories();
+    this.updateAllCheckableCategories();
   }
 
   @Output() onCancel = new EventEmitter<void>();
   @Output() onSave = new EventEmitter<ICompetitionCategory[]>();
 
   allCheckableCategories: CheckableCategory[];
+  shownCheckableCategories: CheckableCategory[];
 
   private _allCategories: ICompetitionCategory[];
   private _selectedCategories: ICompetitionCategory[];
 
-  constructor() { }
+  private _filterKeyword: string;
+
+  filterFormGroup: FormGroup;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.filterFormGroup = new FormGroup({
+      filter: new FormControl(null)
+    });
   }
 
   cancel() {
@@ -43,7 +54,7 @@ export class AddCategoriesModalComponent implements OnInit {
     this.onSave.emit(checkedCategories);
   }
 
-  updateCheckedCategories() {
+  updateAllCheckableCategories() {
     let selectedByCategoryId = {};
 
     if (this._selectedCategories) {
@@ -53,7 +64,28 @@ export class AddCategoriesModalComponent implements OnInit {
     }
 
     this.allCheckableCategories = this._allCategories.map((category: ICompetitionCategory) =>
-      new CheckableCategory(!!selectedByCategoryId[category.id], category));
+      new CheckableCategory(selectedByCategoryId[category.id] === true, category));
+
+    this.updateShownCategories();
+  }
+
+  filterCompetitions(): void {
+    this._filterKeyword = this.filterFormGroup.value.filter;
+
+    this.updateShownCategories();
+  }
+
+  updateShownCategories() {
+    const filterKeyword: string = this._filterKeyword ? this._filterKeyword.toLowerCase() : null;
+
+    this.shownCheckableCategories = this._filterKeyword && this._filterKeyword.length > 0 ?
+      this.allCheckableCategories.filter((cat) => this.isNameMappedToFilterKeyword(cat.category.displayName.toLowerCase(), filterKeyword)) :
+      this.allCheckableCategories;
+  }
+
+  isNameMappedToFilterKeyword(displayName: string, filterKeyword: string): boolean {
+    const filterKeywords: string[] = filterKeyword.split(' ');
+    return filterKeywords.find((filterWord) => displayName.indexOf(filterWord) < 0) === undefined;
   }
 }
 
@@ -61,7 +93,7 @@ class CheckableCategory {
   isChecked: boolean;
   category: ICompetitionCategory;
 
-  constructor(isChecked: boolean, category: ICompetitionCategory){
+  constructor(isChecked: boolean, category: ICompetitionCategory) {
     this.isChecked = isChecked;
     this.category = category;
   }
