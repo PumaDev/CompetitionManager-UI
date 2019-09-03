28c65f21a38def50d2f4bbadfc06ee306d49872e
PumaDev <mihail_puzdrov@mail.ru>
2019-09-03 22:52:46 +0300

    Enhance deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@
 # Build application for prod env
 ng build --prod --aot
 
+# Clear Bucket
+aws s3 rm s3://mke-competition-manager/* --recursive
+
 # Upload files
 aws s3 cp ./dist/CompetitionManager-UI/ s3://mke-competition-manager --recursive --acl public-read
