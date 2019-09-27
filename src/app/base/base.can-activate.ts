fab32d208a7c55e5910d359daee312a406f922f3
PumaDev <mihail_puzdrov@mail.ru>
2019-09-27 21:27:44 +0300

    Fix Bug: When token expires system not logout user

--- src/app/base/base.can-activate.ts
+++ src/app/base/base.can-activate.ts
@@ -1,8 +1,8 @@
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
-import { AuthService } from '../auth/services/auth.service';
-import { IAccessToken } from '../shared/permissions/models/permission.models';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {AuthService} from '../auth/services/auth.service';
+import {IAccessToken} from '../shared/permissions/models/permission.models';
 
 @Injectable()
 export class CanActivateBasePage implements CanActivate {
@@ -14,11 +14,22 @@ export class CanActivateBasePage implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     const accessToken: IAccessToken = JSON.parse(localStorage.getItem('access-token'));
     if (accessToken) {
-      return true;
+      if (new Date(accessToken.expiresIn).getTime() < this.getGmt0Time()) {
+        localStorage.removeItem('access-token');
+        localStorage.removeItem('user');
+      } else {
+        return true;
+      }
     }
 
     this.router.navigate([this.authService.getLoginUrl()]);
     return false;
+  }
+
+  private getGmt0Time(): number {
+    const currentDate: Date = new Date();
+
+    return currentDate.valueOf() + currentDate.getTimezoneOffset() * 60000;
   }
 
 }
