a34487a4d9543e6c699578a24968d08e206ec95c
PumaDev <mihail_puzdrov@mail.ru>
2020-03-22 13:25:00 +0300

    Fix deploy issue

--- src/app/auth/auth.routers.ts
+++ src/app/auth/auth.routers.ts
@@ -3,22 +3,18 @@ import { LoginComponent } from './components/login/login.component';
 import { CanActivateLoginPage } from './login-page.can-activate';
 import { RegisterSmartComponent } from './components/register/register.smart.component';
 import { LoginMobileComponent } from './components/login/login.mobile.component';
-import { ResponsiveComponents } from '../shared/screen-state/mobile.state';
+import { getComponentForCurrentScreenState } from '../shared/screen-state/mobile.state';
 import { RegisterMobileSmartComponent } from './components/register/mobile/register.mobile.smart.component';
-
-const authResponsiveComponents: ResponsiveComponents = new ResponsiveComponents();
-authResponsiveComponents.add(LoginComponent, LoginMobileComponent);
-authResponsiveComponents.add(RegisterSmartComponent, RegisterMobileSmartComponent);
 
 const routes: Routes = [
   {
     path: 'login',
-    component: authResponsiveComponents.get(LoginComponent),
+    component: getComponentForCurrentScreenState(LoginComponent, LoginMobileComponent),
     canActivate: [CanActivateLoginPage]
   },
   {
     path: 'registration',
-    component: authResponsiveComponents.get(RegisterSmartComponent),
+    component: getComponentForCurrentScreenState(RegisterSmartComponent, RegisterMobileSmartComponent),
     canActivate: [CanActivateLoginPage]
   }
 ];
--- src/app/shared/screen-state/mobile.state.ts
+++ src/app/shared/screen-state/mobile.state.ts
@@ -1,32 +1,7 @@
 import { Type } from '@angular/core';
 
-export class ResponsiveComponents {
-
-    private components: Map<Type<any>, MobileDesctopComponents> = new Map();
-
-    add(rootComponent: Type<any>, mobileComponent: Type<any>, desctopComponent: Type<any> = null) {
-        if (desctopComponent === null) {
-            desctopComponent = rootComponent;
-        }
-        this.components.set(rootComponent, {mobileComponent: mobileComponent, desctopComponent: desctopComponent});
-    }
-
-    get(rootComponent: Type<any>): Type<any> {
-        const foundComponents = this.components.get(rootComponent);
-
-        return foundComponents ? 
-            isMobileVersion() ? 
-                foundComponents.mobileComponent : 
-                foundComponents.desctopComponent :
-            rootComponent;
-    }
-
-}
-
-
-interface MobileDesctopComponents {
-    mobileComponent: Type<any>;
-    desctopComponent: Type<any>;
+export function getComponentForCurrentScreenState(desctopComponentt: Type<any>, mobileComponent: Type<any>): Type<any> {
+    return isMobileVersion() ? mobileComponent : desctopComponentt;
 }
 
 export function isMobileVersion(): boolean {
